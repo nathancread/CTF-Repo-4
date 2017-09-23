1c9ac8972c9f69985ec834908cb74711e2ffcf49
nathancread <31357431+nathancread@users.noreply.github.com>
2017-09-22 19:52:33 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-document.cookie = "FLAG=CookieMonster";
+function o(h) 
+{
+    var s = '';
+    for (var i = 0; i < h.length; i += 2)
+        s += String.fromCharCode(parseInt(h.substr(i, 2), 16));
+    return s;
+}     
+
+document.cookie = "FLAG=" + o("436f6f6b69654d6f6e73746572");
